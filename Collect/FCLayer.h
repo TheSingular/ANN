827d022d60d1d571ede700b7a7854a3b03c565d0
TheSingular <filozzof93@gmail.com>
2023-12-21 16:49:19 +0300

    Second commit

--- Collect/FCLayer.h
+++ Collect/FCLayer.h
@@ -3,15 +3,17 @@ ref class FCLayer
 {
 	public:
 		FCLayer();
+		FCLayer(int numNeurons, int numInputs, FCLayer^ prevLayer, FCLayer^ nextLayer);
+		void updateNumInputs();
+		void updateWeights(double error);
 		~FCLayer();
 
 
 		private:
 			int numNeurons;
-			int numInputs;
+			int numInputs = 2;
 			FCLayer^ prevLayer;
 			FCLayer^ nextLayer;
 			array<double, 2>^ weights;
 			array<double>^ bias;
 };
-
